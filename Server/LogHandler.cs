f9be8acd128802e166cbe5de19093e9425421136
Sam Nicknam <s.nicknam@icloud.com>
2017-10-18 00:27:54 +0200

    Server can send json from stream but still receives entire string and then saves it.

--- Server/LogHandler.cs
+++ Server/LogHandler.cs
@@ -1,9 +1,19 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
 
 namespace Server
 {
     public static class LogHandler
     {
+        private static readonly string AppDataDir = Path.Combine(Environment.GetFolderPath(
+            Environment.SpecialFolder.LocalApplicationData,
+            Environment.SpecialFolderOption.DoNotVerify), Assembly.GetExecutingAssembly().GetName().Name);
+
         /// <summary>
         /// Save a log to the appropriate place in the filesystem.
         /// </summary>
@@ -11,19 +21,162 @@ namespace Server
         /// <returns>Returns the log's unique id to use when requesting it from storage.</returns>
         public static string SaveLog(string log)
         {
-            // TODO create body
-            return null;
+            Directory.CreateDirectory(AppDataDir);
+            var logID = "";
+            var path = "";
+            do
+            {
+                logID = Guid.NewGuid().ToString("D");
+                path = Path.Combine(AppDataDir, logID);
+            } while (File.Exists(path));
+            File.WriteAllText(path, log);
+            return logID;
         }
 
+//        public static string CopyFromStream(Stream stream)
+//        {
+//            var schema = JSchema.Parse(JsonSchemaLog);
+//            using (var s = new StreamReader(stream))
+//            using (var reader = new JSchemaValidatingReader(new JsonTextReader(s)))
+//            {
+//                // assign schema and setup event handler
+//                reader.Schema = schema;
+//                reader.ValidationEventHandler += (sender, args) => throw new JsonException();
+//
+//                // bigdata.json will be validated without loading the entire document into memory
+//                while (reader.Read())
+//                {
+//                }
+//            }
+//        }
+
         /// <summary>
-        /// Load a log from storage.
+        /// Copies the file to the given stream.
         /// </summary>
         /// <param name="logId">The log's unique identifier.</param>
-        /// <returns>Returns the requested log, or null if it doesn't exist.</returns>
-        public static string LoadLog(string logId)
+        /// <param name="stream">The stream to copy the file to.</param>
+        /// <returns>Returns true if the file was loaded and copied correctly.</returns>
+        public static bool CopyToStream(string logId, Stream stream)
         {
-            // TODO create body
-            return null;
+            try
+            {
+                File.OpenRead(Path.Combine(AppDataDir, logId)).CopyTo(stream);
+                stream.Flush();
+                return true;
+            }
+            catch (FileNotFoundException)
+            {
+                return false;
+            }
+            catch (ArgumentException)
+            {
+                return false;
+            }
         }
+
+        #region JSON Schema Log
+
+        private const string JsonSchemaLog = @"{
+	""type"":""object"",
+	""$schema"": ""http://json-schema.org/draft-03/schema"",
+	""id"": ""http://jsonschema.net"",
+	""required"":true,
+	""properties"":{
+		""EnduranceTest"": {
+			""type"":""object"",
+			""id"": ""http://jsonschema.net/EnduranceTest"",
+			""required"":true,
+			""properties"":{
+				""ErgometerLog"": {
+					""type"":""array"",
+					""id"": ""http://jsonschema.net/EnduranceTest/ErgometerLog"",
+					""required"":true,
+					""items"":
+						{
+							""type"":""object"",
+							""id"": ""http://jsonschema.net/EnduranceTest/ErgometerLog/0"",
+							""required"":true,
+							""properties"":{
+								""ActualPower"": {
+									""type"":""number"",
+									""id"": ""http://jsonschema.net/EnduranceTest/ErgometerLog/0/ActualPower"",
+									""required"":true
+								},
+								""Distance"": {
+									""type"":""number"",
+									""id"": ""http://jsonschema.net/EnduranceTest/ErgometerLog/0/Distance"",
+									""required"":true
+								},
+								""HR"": {
+									""type"":""number"",
+									""id"": ""http://jsonschema.net/EnduranceTest/ErgometerLog/0/HR"",
+									""required"":true
+								},
+								""RPM"": {
+									""type"":""number"",
+									""id"": ""http://jsonschema.net/EnduranceTest/ErgometerLog/0/RPM"",
+									""required"":true
+								},
+								""RequestedPower"": {
+									""type"":""number"",
+									""id"": ""http://jsonschema.net/EnduranceTest/ErgometerLog/0/RequestedPower"",
+									""required"":true
+								},
+								""Speed"": {
+									""type"":""number"",
+									""id"": ""http://jsonschema.net/EnduranceTest/ErgometerLog/0/Speed"",
+									""required"":true
+								},
+								""Time"": {
+									""type"":""number"",
+									""id"": ""http://jsonschema.net/EnduranceTest/ErgometerLog/0/Time"",
+									""required"":true
+								}
+							}
+						}
+					
+
+				},
+				""Patient"": {
+					""type"":""object"",
+					""id"": ""http://jsonschema.net/EnduranceTest/Patient"",
+					""required"":true,
+					""properties"":{
+						""BirthDate"": {
+							""type"":""string"",
+							""id"": ""http://jsonschema.net/EnduranceTest/Patient/BirthDate"",
+							""required"":true
+						},
+						""FirstName"": {
+							""type"":""string"",
+							""id"": ""http://jsonschema.net/EnduranceTest/Patient/FirstName"",
+							""required"":true
+						},
+						""LastName"": {
+							""type"":""string"",
+							""id"": ""http://jsonschema.net/EnduranceTest/Patient/LastName"",
+							""required"":true
+						}
+					}
+				},
+				""TestResults"": {
+					""type"":""object"",
+					""id"": ""http://jsonschema.net/EnduranceTest/TestResults"",
+					""required"":true,
+					""properties"":{
+						""VO2Max"": {
+							""type"":""number"",
+							""id"": ""http://jsonschema.net/EnduranceTest/TestResults/VO2Max"",
+							""required"":true
+						}
+					}
+				}
+			}
+		}
+	}
+}
+";
+
+        #endregion
     }
 }
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,8 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Server
 {
@@ -10,7 +17,182 @@ namespace Server
     {
         static void Main(string[] args)
         {
-            // TODO create body
+            const int port = 420;
+            Console.CancelKeyPress += OnCancel;
+            var listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+            Console.WriteLine("Server started\nTime: {0:R}\nEndpoint: {1}\n", DateTime.Now,
+                (IPEndPoint) listener.LocalEndpoint);
+            LogHandler.SaveLog("");
+            while (true)
+            {
+                var c = listener.AcceptTcpClient();
+                var client = new Client(c);
+                new Thread(() => client.Accept()).Start();
+            }
+        }
+
+        /// <summary>
+        /// This method is called on cancel key press.
+        /// </summary>
+        /// <param name="sender">The sender of the cancellation.</param>
+        /// <param name="e">The arguments of the cancellation event.</param>
+        private static void OnCancel(object sender, ConsoleCancelEventArgs e)
+        {
+            Console.WriteLine("Shutting down...");
+            Environment.Exit(0);
+        }
+    }
+
+    internal class Client
+    {
+        private readonly TcpClient _client;
+        private readonly SslStream _stream;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Client"/> class.
+        /// </summary>
+        /// <param name="tcpClient">The TCP client connection to associate with this client.</param>
+        public Client(TcpClient tcpClient)
+        {
+            _client = tcpClient;
+            _stream = new SslStream(_client.GetStream());
+            try
+            {
+                _stream.AuthenticateAsServer(GetCertificate(), false, SslProtocols.Tls12, false);
+            }
+            catch (AuthenticationException)
+            {
+                Close();
+            }
+            catch (IOException)
+            {
+                Close();
+            }
+            catch (Win32Exception e)
+            {
+                if (e.NativeErrorCode == -2146893043)
+                {
+                    Console.WriteLine("Run server as admin.");
+                    Environment.Exit(1);
+                }
+                else
+                    throw;
+            }
+        }
+
+        /// <summary>
+        /// Accept a client's request.
+        /// </summary>
+        public void Accept()
+        {
+            if (!_client.Connected) return;
+            try
+            {
+                var sizeBuffer = new byte[4];
+                _stream.Read(sizeBuffer, 0, sizeBuffer.Length);
+                var messageLength = BitConverter.ToInt32(sizeBuffer, 0);
+                var messageBuffer = new byte[_client.ReceiveBufferSize];
+                var messageBuilder = new StringBuilder();
+                var requestType = (Request) _stream.ReadByte();
+                if (!Enum.IsDefined(typeof(Request), requestType))
+                {
+                    Close();
+                    return;
+                }
+                switch (requestType)
+                {
+                    case Request.Get:
+                        var read = 0;
+                        do
+                        {
+                            read += _stream.Read(messageBuffer, 0, messageBuffer.Length);
+                            messageBuilder.Append(Encoding.ASCII.GetString(messageBuffer));
+                        } while (read < messageLength);
+
+                        var request = JObject.Parse(messageBuilder.ToString());
+                        if (!LogHandler.CopyToStream(request["LogID"].ToObject<string>(), _stream))
+                            Send(ErrorAsJSON(Error.LogNotFound));
+                        break;
+                    case Request.Put:
+                        var logID = LogHandler.SaveLog(messageBuilder.ToString());
+                        Send(logID);
+                        break;
+                }
+            }
+            catch (JsonReaderException)
+            {
+            }
+            catch (IOException)
+            {
+            }
+            catch (Win32Exception e)
+            {
+                if (e.NativeErrorCode == -2147467259)
+                {
+                    Console.WriteLine("Run server as admin.");
+                    Environment.Exit(1);
+                }
+                else
+                    throw;
+            }
+            finally
+            {
+                Close();
+            }
+        }
+
+        /// <summary>
+        /// Send a string to the client.
+        /// </summary>
+        /// <param name="message"></param>
+        private void Send(string message)
+        {
+            var msg = Encoding.ASCII.GetBytes(message);
+            var prefix = BitConverter.GetBytes(msg.Length);
+            var buffer = new byte[prefix.Length + msg.Length];
+            Buffer.BlockCopy(prefix, 0, buffer, 0, prefix.Length);
+            Buffer.BlockCopy(msg, 0, buffer, prefix.Length, msg.Length);
+            _stream.Write(buffer, 0, buffer.Length);
+        }
+
+        /// <summary>
+        /// Close the TCP client connection and network stream.
+        /// </summary>
+        private void Close()
+        {
+            _client.Close();
+            _stream.Close();
+        }
+
+        /// <summary>
+        /// Get the SSL certificate from store.
+        /// </summary>
+        /// <returns>Returns the SSL certificate.</returns>
+        private static X509Certificate GetCertificate()
+        {
+            const string certThumbprint = "5D4E584F97C51BA721CDDC00A17981F7FCC0084E";
+            var store = new X509Store(StoreName.My, StoreLocation.LocalMachine);
+            store.Open(OpenFlags.ReadOnly);
+            var cert = store.Certificates.Find(X509FindType.FindByThumbprint, certThumbprint, true)[0];
+            store.Close();
+            if (cert != null) return cert;
+            Console.WriteLine("The certificate could not be found.");
+            Environment.Exit(1);
+            return cert;
+        }
+
+        private string ErrorAsJSON(Error error) => $@"{{""Error:{(int) error}}}";
+
+        internal enum Error
+        {
+            LogNotFound
+        }
+
+        internal enum Request
+        {
+            Put,
+            Get
         }
     }
 }
